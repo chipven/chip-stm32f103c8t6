e15f77600437c74c72d0c80cf382f4d63d2f3053
evenardo <evenardo@gmail.com>
2018-10-14 15:05:26 +0800

    implement uart rx use tim & exti

--- src/main.c
+++ src/main.c
@@ -5,7 +5,7 @@ int numberToShow = 0;
 unsigned char uartBuffer;
 unsigned int *rx = B1_in;
 unsigned int *tx = B0_out;
-int uartCount = 0;
+
 int main()
 {
     on_72m();
@@ -41,7 +41,6 @@ int main()
     TIM2->DIER = 0x1;
     TIM2->CR1 |= 0x1 << 7;
     TIM2->CR1 |= 0x1 << 4;
-
     while (1)
     {
         // device_8digi_show(d8, ven_tim.acc);
@@ -49,35 +48,50 @@ int main()
     }
 }
 
+//uart 专用计数器
+int uartCount = 0;
 void TIM2_IRQHandler()
 {
+    //计数器加1
     uartCount++;
+    //如果计数器为1 等待*rx的下拉信号
     if (uartCount == 1)
     {
         while (*rx != 0)
             ;
     }
+    //如果是第2至9位,收取8位rx的信号;
     if (2 <= uartCount && uartCount <= 9)
     {
         uartBuffer >>= 1;
         uartBuffer |= *rx << 7;
     }
+    //如果是第十位,等待停止位;
     if (uartCount == 10)
     {
         while (*rx != 1)
             ;
-        numberToShow = numberToShow << 8 | uartBuffer;
-        EXTI->IMR |= 0x1 << 1;
+        //要显示的内容向左移8位;
+        numberToShow <<= 8;
+        //把buffer的内容或进最后8位;
+        numberToShow |= uartBuffer;
+        //关闭TIM2使能
         TIM2->CR1 &= 0x0 << 0;
+        //开启EXTI1使能
+        EXTI->IMR |= 0x1 << 1;
     }
-    //
+    //改写TIM2更新标志
     TIM2->SR &= ~(1 << 0);
 }
 
 void EXTI1_IRQHandler()
 {
-    uartCount = 0;
-    EXTI->IMR &= 0x0 << 1;
+    //打开TIM2定时器
     TIM2->CR1 |= 0x1 << 0;
+    //uart计数器归零
+    uartCount = 0;
+    //关闭EXTI1使能
+    EXTI->IMR &= 0x0 << 1;
+    //取反外部中断挂起位
     EXTI->PR |= 0x1 << 1;
 }
