87b4f7d4916f3cd1fd402c817e37e94c00b076e2
evenardo <evenardo@gmail.com>
2018-10-16 00:17:42 +0800

    need resolve first char always wrong when restart chip

--- src/main.c
+++ src/main.c
@@ -3,8 +3,8 @@
 #include "../ven/system_util.h"
 unsigned int *rx = B1_in;
 unsigned int *tx = B0_out;
-unsigned char rxBuffer;
-unsigned char txBuffer;
+unsigned char rxBuffer = 0xff;
+unsigned char txBuffer = 0xff;
 unsigned char txCount = 0;
 unsigned char rxCount = 0;
 // unsigned char received = 0;
@@ -14,10 +14,42 @@ void send(unsigned char data)
     txBuffer = data;
     TIM3->CR1 |= 0x1 << 0; //开始计数
 }
+void uartInit()
+{
+    // 外部中断启用
+    NVIC->ISER[0] |= 0x1 << 7;   //开启EXTI1,中断向量为7
+    RCC->APB2ENR |= 0x1 << 3;    //IOPBEN=1
+    RCC->APB2ENR |= 0x1 << 0;    //AFIOEN=1
+    GPIOB->CRL &= 0xffffff00;    //与零
+    GPIOB->CRL |= 0x00000042;    //b0 推挽输出, b1 推挽输入
+    AFIO->EXTICR[0] |= 0x1 << 4; //EXTI1的关联GPIO为GPIOB
+    EXTI->FTSR |= 0x1 << 1;      //EXTI1开启下降沿检测
+    EXTI->IMR |= 0x1 << 1;       //EXTI1的中断屏蔽打开
 
+    //配置一个RX定时器
+    NVIC->ISER[0] |= 0x1 << 28; //开启TIM2中断向量28
+    RCC->APB1ENR |= 0x1 << 0;   //TIM2EN=1,开启APB1的TIM2EN
+    TIM2->PSC = 71;             //预分频
+    TIM2->ARR = 102;            //预装载
+    TIM2->DIER = 0x1;           //DMA中断使能寄存器UIE允许更新中断
+    TIM2->CR1 |= 0x1 << 7;      //ARPE=1允许自动装载
+    TIM2->CR1 |= 0x1 << 4;      //向下计数
+    // TIM2->CR1 |= 0x1 << 0;
+    // TIM2->CR1 &= ~(0x1 << 0);
+
+    //配置一个TX定时器
+    NVIC->ISER[0] |= 0x1 << 29; //开启TIM3中断向量29
+    RCC->APB1ENR |= 0x1 << 1;   //TIM3EN=1
+    TIM3->PSC = 71;             //预分频
+    TIM3->ARR = 102;            //预装载
+    TIM3->DIER |= 0x1 << 0;     //UIE=1 允许更新中断
+    TIM3->CR1 |= 0x1 << 7;      //允许ARR载入
+    TIM3->CR1 |= 0x1 << 4;      //向下计数
+}
 int main()
 {
     on_72m();
+    uartInit();
 
     //配置led管的针脚
     Device_8digi d8;
@@ -34,37 +66,8 @@ int main()
     d8.chip_74hc595.shiftClock = B14_out;
 
     //要显示的数据
-    numberToShow = 0xff;
-
-    // 外部中断启用
-    NVIC->ISER[0] |= 0x1 << 7;   //开启EXTI1,中断向量为7
-    RCC->APB2ENR |= 0x1 << 3;    //IOPBEN=1
-    RCC->APB2ENR |= 0x1 << 0;    //AFIOEN=1
-    GPIOB->CRL &= 0xffffff00;    //与零
-    GPIOB->CRL |= 0x00000083;    //b0 推挽输出, b1 推挽输入
-    AFIO->EXTICR[0] |= 0x1 << 4; //EXTI1的关联GPIO为GPIOB
-    EXTI->IMR |= 0x1 << 1;       //EXTI1的中断屏蔽打开
-    EXTI->FTSR |= 0x1 << 1;      //EXTI1开启下降沿检测
-
-    //配置一个RX定时器
-    NVIC->ISER[0] |= 0x1 << 28; //开启TIM2中断向量28
-    RCC->APB1ENR |= 0x1 << 0;   //TIM2EN=1,开启APB1的TIM2EN
-    TIM2->PSC = 71;             //预分频
-    TIM2->ARR = 100;            //预装载
-    TIM2->DIER = 0x1;           //DMA中断使能寄存器UIE允许更新中断
-    TIM2->CR1 |= 0x1 << 7;      //ARPE=1允许自动装载
-    TIM2->CR1 |= 0x1 << 4;      //向下计数
-
-    //配置一个TX定时器
-    NVIC->ISER[0] |= 0x1 << 29; //开启TIM3中断向量29
-    RCC->APB1ENR |= 0x1 << 1;   //TIM3EN=1
-    TIM3->PSC = 71;             //预分频
-    TIM3->ARR = 100;            //预装载
-    TIM3->DIER |= 0x1 << 0;     //UIE=1 允许更新中断
-    TIM3->CR1 |= 0x1 << 7;      //允许ARR载入
-    TIM3->CR1 |= 0x1 << 4;      //向下计数
-
-    // *tx = 1;
+    unsigned int numberToShow = 0xff;
+    // send(0xaa);
 
     int numberFlag = 0;
     while (1)
@@ -81,11 +84,9 @@ int main()
             //把buffer的内容或进最后8位;
             numberToShow |= rxBuffer;
         }
-        // if (txCount == 0 && rxReceivedFlag == 1)
         if (txCount == 0 && rxCount == 9)
         {
             send(rxBuffer);
-            // rxReceivedFlag = 0;
         }
         if (rxCount == 0)
         {
@@ -119,8 +120,6 @@ void TIM2_IRQHandler()
 
         //关闭TIM2使能
         TIM2->CR1 &= 0x0 << 0;
-        //开启EXTI1使能
-        EXTI->IMR |= 0x1 << 1;
         rxCount = 0;
     }
     //改写TIM2更新标志
@@ -131,10 +130,6 @@ void EXTI1_IRQHandler()
 {
     //打开TIM2定时器
     TIM2->CR1 |= 0x1 << 0;
-    //uart计数器归零
-    rxCount = 0;
-    //关闭EXTI1使能
-    EXTI->IMR &= 0x0 << 1;
     //取反外部中断挂起位
     EXTI->PR |= 0x1 << 1;
 }
@@ -147,16 +142,17 @@ void TIM3_IRQHandler()
     {
         *tx = 0;
     }
-    if (txCount >= 2 && txCount <= 9)
+    if (txCount > 1 && txCount < 10)
     {
         *tx = txBuffer & 1;
         txBuffer >>= 1;
     }
-    if (txCount >= 10)
+    if (txCount == 10)
     {
         *tx = 1;
         txCount = 0;
-        TIM3->CR1 &= ~(0x1 << 0);
+        TIM3->CR1 &= ~(0x1 << 0); //关闭TIM3的CEN
     }
+    //改写TIM2更新标志
     TIM3->SR &= ~(0x1 << 0);
 }
