6ec98602754ce70e684898f27db2cd035031d537
evenardo <evenardo@gmail.com>
2018-10-23 11:35:33 +0800

    add uart function to system_util

--- src/main.h
+++ src/main.h
@@ -1,4 +1,3 @@
 #ifndef MAIN_H
 #define MAIN_H
-#include "../make/stm32f103c8t6.h"
 #endif /* MAIN_H */
--- ven/system_util.h
+++ ven/system_util.h
@@ -1,10 +1,11 @@
 #ifndef VE_SYS_H
 #define VE_SYS_H
 #include "../make/stm32f103c8t6.h"
-void
-on_72m();
-void
-on_gpio_pp();
-void
-delay();
+void on_72m();
+void on_gpio_pp();
+void delay();
+void openUart1();
+void uart1SendByte(unsigned char data);
+void uart1SendString(char *cmd);
+void open72m();
 #endif /* VE_SYS_H */
